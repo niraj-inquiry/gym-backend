556ec720c65e1d74e41643d0f384379743f610e1
Niraj Kumar <niraj.inquiry@gmail.com>
2023-06-27 22:58:45 +0530

    Update userRoutes.js

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -2,7 +2,7 @@
 const express=require('express')
 const bcrypt = require('bcrypt');
 const router=express.Router()
-const userController=require('../controllers/usersController');
+const {update_userprofile,delete_users_details, get_userdata_byid,register, login,get_all_user,forget_password}=require('../controllers/usersController');
 const verifyToken = require('../../middleware/auth');
 
 const upload = require('../../utils/uploadImage');
@@ -12,12 +12,12 @@ const upload = require('../../utils/uploadImage');
 router.get('/getdata',(req,res)=>{
     res.status(200).send("dddd")
 })
-router.get('/get-all-users',userController.get_all_user)
-router.post('/register',upload.single('image'),userController.register)
+router.get('/get-all-users',get_all_user)
+router.post('/register',upload.single('image'),register)
 router.post('/login',userController.login)
-router.get("/get-data-by-userid/:user_id",userController.get_userdata_byid)
-router.delete('/delete-user-details/:user_id',userController.delete_users_details)
+router.get("/get-data-by-userid/:user_id",get_userdata_byid)
+router.delete('/delete-user-details/:user_id',delete_users_details)
 // router.put('/user-verify-by-admin',userController.verify_vendor_by_admin)
-router.put('/update-user',upload.single('image'),userController.update_userprofile)
-router.post('/forget-password',userController.forget_password)
+router.put('/update-user',upload.single('image'),update_userprofile)
+router.post('/forget-password',forget_password)
 module.exports=router
